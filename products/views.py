1c6ad47339dab06945aa1d4c4faa5b2bcf705b9e
Dominikwigh <dominik-00@hotmail.com>
2023-03-15 08:25:14 +0000

    added category sorting to all products view

--- products/views.py
+++ products/views.py
@@ -10,25 +10,33 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    category = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(
-                    request, "You haven't entered anything"
+                    request, "You didn't enter anything!!"
                     )
                 return redirect(reverse('products'))
-            
+
             queries = Q(name__icontains=query) | Q(
                 description__icontains=query
                 )
             products = products.filter(queries)
-    
+
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
+
     return render(request, 'products/products.html', context)
 
 
