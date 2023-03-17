0bbdf2fd8cc8607b255c23d4e54cb210aa1eb92e
Dominikwigh <dominik-00@hotmail.com>
2023-03-17 12:02:42 +0000

    added stripe js script and public key

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MmbifEGL49n1U78UdJX0RSh3utAaUqEd9rZUyRIQgTEhXRDBtzd5cqq6Xk02ay7Cq3XHo1hsSMZeIUt4TYXKHhE00dDNzkQb8',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
