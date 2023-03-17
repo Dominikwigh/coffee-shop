8c5a59531e37db83305a8990a21d623eca193732
Dominikwigh <dominik-00@hotmail.com>
2023-03-17 12:21:46 +0000

    added validation script and installed stripe

--- checkout/views.py
+++ checkout/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, redirect, reverse 
 from .forms import OrderForm
+from django.contrib import messages
+from bag.contexts import bag_contents
 
 
+# Checkout view
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
-
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
