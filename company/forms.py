71b2664b8afda718e861d6bc598cb780d88441e6
Dominikwigh <dominik-00@hotmail.com>
2023-03-21 08:57:59 +0000

    created contact model

--- company/forms.py
+++ company/forms.py
@@ -4,11 +4,11 @@ from .models import Contact
 
 class ContactForm(forms.ModelForm):
     """ Fields for contact form """
-    name = forms.Charfield(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.Emailfield(max_length=100, widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    subject = forms.Charfield(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    message = forms.Charfield(widget=forms.TextArea(attrs={'class': 'form-control', 'placeholder': 'Write your message here!'}))
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(max_length=100, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    subject = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Write your message here!'}))
 
     class Meta:
         model = Contact
-        fields = ('name', 'emial', 'subject', 'message')
+        fields = ('name', 'email', 'subject', 'message')
--- company/models.py
+++ company/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Contact(models.Model):
+    """ Model for contact form """
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    subject = models.CharField(max_length=255)
+    message = models.CharField(max_length=1000)
+    created_on = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        verbose_name = 'Contact Form'
+        ordering = ['-created_on']
