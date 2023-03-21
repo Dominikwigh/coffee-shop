2e77076e6619a4f0b3a1a4fc00349e3d015c0b6b
Dominikwigh <dominik-00@hotmail.com>
2023-03-21 13:46:19 +0000

    created newsletter form

--- company/forms.py
+++ company/forms.py
@@ -12,3 +12,16 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ('name', 'email', 'subject', 'message')
+
+
+class NewsletterForm(forms.Modelform):
+    class Meta:
+        model = Newsletter
+        fields = ['name', 'email']
+
+    def __init__(self, *args, **kwargs):
+        super(NewsletterForm, self).__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': 'Eneter your Name..'})
+        self.fields['email'].widget.attrs.update(
+            {'class': 'form-control', 'placeholder': 'Eneter your Email..'})
