324184cc2c6564e4d7fdc79e609e93157996a5f4
Dominikwigh <dominik-00@hotmail.com>
2023-03-27 17:07:51 +0000

    tested user profile form and and metaclass

--- profiles/tests/test_models.py
+++ profiles/tests/test_models.py
@@ -19,20 +19,15 @@ class TestUserProfile(TestCase):
 
     def test_create_user_profile_receiver(self):
         """Test UserProfile create receiver"""
-        # Create new standard user
         newuser = User.objects.create_user(
             'unittestuser2', 'unittestuser2@test.com', 'unittestuser2password')
         user_profile = get_object_or_404(UserProfile, user=newuser)
-        # Update user
         self.assertTrue(user_profile)
 
     def test_update_user_profile_receiver(self):
         """Test UserProfile update receiver"""
-        # Get standard user
         user = get_object_or_404(User, username="unittestuser")
-        # Update username
         user.username = "unittestuserupdated"
         user.save()
         user_profile = get_object_or_404(UserProfile, user=user)
-        # Test updated user
         self.assertTrue(user_profile)
--- profiles/tests/test_views.py
+++ profiles/tests/test_views.py
@@ -12,33 +12,9 @@ class TestProfileView(TestCase):
         show profile
         """
         user = User.objects.create_user(
-            'testuser', 'testuser@test.com', 'testuserpassword'
+            'unittestuser', 'unittestuser@test.com', 'unittestuserpassword'
         )
         self.client.force_login(user, backend=None)
         response = self.client.get('/profile/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('profiles/profile.html')
-
-    def test_order_history(self):
-        """
-        Testing order details view
-        by creating test order and accesing URL.
-        """
-        order = Order.objects.create(
-            full_name='testuser',
-            email='testuser@test.com',
-            phone_number='123456',
-            postcode='123456',
-            town_or_city='testtown',
-            street_address1='Address1',
-            street_address2='Address2',
-            county='testcounty',
-            grand_total=17.59,
-            delivery_cost=1.60,
-            order_total=15.99
-        )
-        response = self.client.get(
-            f'/profile/order_history/{order.order_number}/'
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed('checkout/checkout_success.html')
