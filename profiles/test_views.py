c8889f2ab997f2002e0e01abc0f07ab6ba4d22bb
Dominikwigh <dominik-00@hotmail.com>
2023-03-27 17:47:53 +0000

    reconfigured test views in profiles app

--- profiles/test_views.py
+++ profiles/test_views.py
@@ -1,20 +1,37 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from checkout.models import Order
+from django.shortcuts import get_object_or_404
+from django.contrib.messages import get_messages
+
+from .models import UserProfile
 
 
 class TestProfileView(TestCase):
     """ Test Cases """
+    def setUp(self):
+        """
+        Set up info needed for testing
+        """
+
+        self.test_user = User.objects.create_user(
+            username='testname',
+            email='test@test.com',
+            password='password',
+        )
+
+        self.test_user_profile = get_object_or_404(UserProfile,
+                                                   user=self.test_user)
 
     def test_show_profile(self):
         """
-        Test case to register, login and 
-        show profile
+        Test that user can access profile page when logged in
         """
-        user = User.objects.create_user(
-            'unittestuser', 'unittestuser@test.com', 'unittestuserpassword'
-        )
-        self.client.force_login(user, backend=None)
+        self.client.force_login(self.test_user)
         response = self.client.get('/profile/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('profiles/profile.html')
+        self.client.logout()
+
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('accounts/login/')
