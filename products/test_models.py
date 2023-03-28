08c67a4de07bd72593600518e510de152160c1dd
Dominikwigh <dominik-00@hotmail.com>
2023-03-28 08:54:39 +0000

    created tests for category model in products app

--- products/test_models.py
+++ products/test_models.py
@@ -17,3 +17,20 @@ class TestProductModels(TestCase):
             product.description, (
                 "The Lavazza coffee beans are a blend of Arabica from Brazil, RainForest certified. Enjoy a strong coffee with gourmet notes of cereals and dried fruit. 1kg."))
         self.assertNotEqual(product.description, 'test description')
+
+
+class TestCategoryModels(TestCase):
+    fixtures = ['categories.json']
+
+    def test_category_name(self):
+        category = Category.objects.get(pk=1)
+        self.assertEqual(category.name, 'Beans')
+        self.assertNotEqual(category.name, 'test')
+        self.assertEqual(str(category), category.name)
+   
+    def test_category_friendly_name(self):
+        category = Category.objects.get(pk=5)
+        self.assertEqual(category.friendly_name, 'Beans')
+        self.assertNotEqual(category.friendly_name, 'Test Category')
+        self.assertEqual(
+            Category.get_friendly_name(category), category.friendly_name)
